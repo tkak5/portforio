90f92b0c3fb33c60bcc54e6b84c827bac32181cf
三輪貴陽 <tkak@sanrinkyounoMBP.AirPort>
2020-06-12 18:03:16 +0900

    design

--- pages/index.js
+++ pages/index.js
@@ -19,12 +19,13 @@ export default function Home({ allWorksData }) {
         <p className="text-center">1995.5.5</p>
         <a href="https://github.com/tkak5" target="blank"><p className="text-center text-blue-500">github</p></a>
       </div>
+      <h1 className="p-4 text-lg font-bold text-center">ポートフォリオ</h1>
       <ul className="flex flex-wrap">
         {allWorksData.map(({id, title, category, techs, url, photo, git, comment}) => (
-          <li id={id} className="p-4">
-            <div className="h-full border-2 border-gray-200 rounded-lg overflow-hidden md:flex">
+          <li id={id} className="p-4 md:w-1/2">
+            <div className="h-full border-2 border-gray-200 rounded-lg overflow-hidden">
               <div className="p-6">
-                <p>{category}({title})</p>
+                <p className="text-lg">{category}({title})</p>
                 <ul className="my-2">
                   <p className="font-bold">使っている技術</p>
                   {techs.map((tech) => (
@@ -35,7 +36,7 @@ export default function Home({ allWorksData }) {
                 <a target="blank" href={git} className="text-blue-500"><p>source code</p></a>
                 <p>{comment}</p>
               </div>
-              <div className="w-full object-cover object-center md:border-l-2">
+              <div className="w-full object-cover object-center">
                 <a href={url} target="blank"><img src={photo}/></a>
               </div>
             </div>
